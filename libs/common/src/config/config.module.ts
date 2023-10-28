fe7d1131ff2b2025e49d28c3672e969f3f8da8b3
Ehsan Moallaee <ehsanm78@gmail.com>
2023-10-28 04:59:29 +0330

    Joi package installed and use to validate our configModule to get MONGODB_URI value from .env file correctly

--- libs/common/src/config/config.module.ts
+++ libs/common/src/config/config.module.ts
@@ -1,8 +1,13 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule as NestConfigModule, ConfigService } from '@nestjs/config';
+import * as Joi from 'joi';
 
 @Module({
-  imports: [NestConfigModule.forRoot()],
+  imports: [NestConfigModule.forRoot({
+    validationSchema: Joi.object({
+        MONGODB_URI: Joi.string().required(),
+    })
+  })],
   providers: [ConfigService],
   exports: [ConfigService]
 })
