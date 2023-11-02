a47fee8e4b3304e38a5fd9fb0325510591f8f837
Ehsan Moallaee <ehsanm78@gmail.com>
2023-11-03 02:19:47 +0330

    Passport local strategy and login route done

--- apps/auth/src/auth.controller.ts
+++ apps/auth/src/auth.controller.ts
@@ -1,12 +1,22 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Post, UseGuards, Res } from '@nestjs/common';
+import { Response } from 'express';
 import { AuthService } from './auth.service';
+import { LocalAuthGuard } from './guards/local-auth.guard';
+import { currentUser } from './decorator/current-user.decorator';
+import { UserDocument } from './users/models/user.schema';
 
-@Controller()
+@Controller('auth')
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
 
-  @Get()
-  getHello(): string {
-    return this.authService.getHello();
+  @UseGuards(LocalAuthGuard)
+  @Post('login')
+  async login(
+    @currentUser() user: UserDocument,
+    @Res({ passthrough: true }) response: Response
+  ) {
+    await this.authService.login(user, response);
+    response.send(user);
   }
+
 }
