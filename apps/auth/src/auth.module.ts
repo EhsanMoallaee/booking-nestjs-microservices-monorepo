6bd9dd9e647cc9e046806c9291f66ce8cebce3a9
Ehsan Moallaee <ehsanm78@gmail.com>
2023-11-02 22:34:31 +0330

    Import and config JwtModule in auth.module.ts and make seperate .env file for each app

--- apps/auth/src/auth.module.ts
+++ apps/auth/src/auth.module.ts
@@ -1,11 +1,20 @@
+import { ConfigService } from '@nestjs/config';
+import { JwtModule } from '@nestjs/jwt';
 import { Module } from '@nestjs/common';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
-import { UsersModule } from './users/users.module';
 import { LoggerModule } from '@app/common';
+import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [UsersModule, LoggerModule],
+  imports: [UsersModule, LoggerModule, JwtModule.registerAsync({
+    useFactory: (configService: ConfigService) => ({
+      secret: configService.get<string>('JWT_SECRET'),
+      signOptions: {
+        expiresIn: `${configService.get('JWT_EXPIRATION')}s`
+      }
+    })
+  })],
   controllers: [AuthController],
   providers: [AuthService],
 })
