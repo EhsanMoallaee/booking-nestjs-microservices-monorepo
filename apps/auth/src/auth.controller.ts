82b90e4094fbb28bb22d629f2178ecd92ad96b3e
Ehsan Moallaee <ehsanm78@gmail.com>
2023-11-18 14:21:38 +0330

    jwt-auth.guard.ts created in common library to use for authenticating requests in other apps

--- apps/auth/src/auth.controller.ts
+++ apps/auth/src/auth.controller.ts
@@ -1,22 +1,27 @@
 import { Controller, Post, UseGuards, Res } from '@nestjs/common';
+import { MessagePattern } from '@nestjs/microservices';
 import { Response } from 'express';
 import { AuthService } from './auth.service';
 import { LocalAuthGuard } from './guards/local-auth.guard';
 import { currentUser } from './decorator/current-user.decorator';
 import { UserDocument } from './users/models/user.schema';
+import { JwtAuthGuard } from '@app/common';
 
 @Controller('auth')
 export class AuthController {
-  constructor(private readonly authService: AuthService) {}
+    constructor(private readonly authService: AuthService) {}
 
-  @UseGuards(LocalAuthGuard)
-  @Post('login')
-  async login(
-    @currentUser() user: UserDocument,
-    @Res({ passthrough: true }) response: Response
-  ) {
-    await this.authService.login(user, response);
-    response.send(user);
-  }
+    @UseGuards(LocalAuthGuard)
+    @Post('login')
+    async login(
+        @currentUser() user: UserDocument,
+        @Res({ passthrough: true }) response: Response
+    ) {
+        await this.authService.login(user, response);
+        response.send(user);
+    }
 
+    @UseGuards(JwtAuthGuard)
+    @MessagePattern('authenticate')
+    async authenticate() {}
 }
--- apps/auth/src/strategies/jwt.strategy.ts
+++ apps/auth/src/strategies/jwt.strategy.ts
@@ -1,6 +1,5 @@
 import { Injectable } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
-import { Request } from 'express';
 import { PassportStrategy } from '@nestjs/passport';
 import { ExtractJwt, Strategy } from 'passport-jwt';
 import { UsersService } from '../users/users.service';
@@ -14,7 +13,8 @@ export class JwtStrategy extends PassportStrategy(Strategy) {
     ) {
         super({
             jwtFromRequest: ExtractJwt.fromExtractors([
-                (request: Request) => request?.cookies?.Authentication
+                (request: any) =>
+                    request?.cookies?.Authentication || request?.Authentication
             ]),
             secretOrKey: configService.get('JWT_SECRET')
         });
